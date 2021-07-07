007ea4276c6e7270806096463e02c4720a4699af
samiam567 <35432488+samiam567@users.noreply.github.com>
2021-07-06 23:21:10 -0400

    attempted to get filter function to work properly

--- ML_qubit_java/src/data_generation/Data_generation.java
+++ ML_qubit_java/src/data_generation/Data_generation.java
@@ -1,17 +1,31 @@
 package data_generation;
 
 import calculatorv2_core.Equation;
+import calculatorv2_core.Equation.DegOrRadValue;
 
 public abstract class Data_generation {
 	
 	public static void main(String[] args) {
 		
 		Equation eq = new Equation();
+		eq.setDegRadMode(DegOrRadValue.radians);
 		eq.importAll();
+		eq.importStandardConstants();
 		
-		String output = eq.calculate("run(\"ml_qubit.cobra\")");
+		eq.calculate("/eq = \"square( abs(  1 + exp(i*w*t) - 2 * exp(i*w*(t+tpi)/2 ) * cos(w*tpi/2)  ) )\"");
 		
-		System.out.println(output);
+		eq.calculate("/tpi = 100E_9");
+		eq.calculate("/w = 100");
+		eq.calculate("/t = 1E_6");
+
+		eq.calculate("graph(eq,\"w\",0,6E7,16)");
+
+
+		eq.calculate("graph(eq,\"w\",0,6E6,1E_8)");
+
+		eq.calculate("graph('integrate(eq,\"w\",0,3000,0.1)',\"t\")");
+		
+
 		
 	
 	}
