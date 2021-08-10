38e52dd4058ce42be27ea5ad93d6313fb5386d7b
samiam567 <35432488+samiam567@users.noreply.github.com>
2021-08-10 19:53:03 -0400

    Added normalization for training data, greatly decreasing loss
    However, I believe that this may look better than it is

--- data_generation_saver.py
+++ data_generation_saver.py
@@ -6,11 +6,27 @@ list_size = 20;
 x_set = [];
 y_set = [];
 
-ttwonaught = 343e-6;
-for i in range(1,100):
-    graphset = data_generator.get_graphset(2,(ttwonaught) * i**2 * 0.5);
+#ttwonaught = 343e-6;
+for ttwo in np.linspace(1e-6,30e-6,100):
+    graphset = data_generator.get_graphset(1,ttwo);
     x_set.append(graphset[0]);
     y_set.append(graphset[1]);
+
+
+def normalize(arr, t_min, t_max):
+    norm_arr = []
+    diff = t_max - t_min
+    diff_arr = np.max(arr) - np.min(arr)    
+    for i in arr:
+        temp = (((i - np.min(arr))*diff)/diff_arr) + t_min
+        norm_arr.append(temp)
+    return norm_arr
+  
+
+x_set = normalize(x_set,0,1);
+
+y_set = normalize(y_set,0,1);
+
 
 full_dataset = [];
 full_dataset.append(x_set);
